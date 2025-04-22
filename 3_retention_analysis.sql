b20b188989f04f8ea8b6428c08d081d7e76b5618
sup25 <suparnaa51@gmail.com>
2025-04-22 23:41:36 +0545

    retention_analysis

---
+++ 3_retention_analysis.sql
@@ -0,0 +1,36 @@
+WITH customer_last_purchase AS (
+    SELECT
+        customerkey,
+        cleaned_name,
+       orderdate,
+        ROW_NUMBER() OVER (PARTITION BY customerkey ORDER BY cohort_year DESC) AS rn,
+        first_purchase_date,
+        cohort_year
+    FROM cohort_analysis
+), churned_customers as(
+    SELECT
+        customerkey,
+        cleaned_name,   
+        orderdate AS last_purchase_date,
+        CASE
+            WHEN orderdate <(SELECT MAX(orderdate) FROM sales) - INTERVAL '6 months' THEN 'Churned'
+            ELSE 'Active' END AS customer_status,
+        cohort_year
+    FROM customer_last_purchase
+    WHERE rn = 1
+        AND first_purchase_date <(SELECT MAX(orderdate) FROM sales) - INTERVAL '6 months'
+)
+SELECT
+    cohort_year
+    customer_status,
+    COUNT(customerkey) AS customer_count,
+    SUM(COUNT(customerkey)) OVER (PARTITION BY cohort_year) AS total_customers,
+    ROUND(COUNT(customerkey) /SUM(COUNT(customerkey)) OVER (PARTITION BY cohort_year) * 100, 2) AS status_percentage --COUNT(customerkey) /SUM(COUNT(customerkey)) OVER () * 100 AS status_percentage
+FROM churned_customers
+GROUP BY 
+    cohort_year,
+    customer_status
+
+
+
+
